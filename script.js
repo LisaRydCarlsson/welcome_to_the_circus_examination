6540a1cf86a1ce57d71a9e4e6d46d4a0a48924ba
Lisa Ryd-Carlsson <117908955+LisaRydCarlsson@users.noreply.github.com>
2023-10-24 16:17:15 +0200

    Added html, css and js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Hämta elementet med id "enter"
+const enterSection = document.getElementById('enter');
+
+// Skapa ett h2-element för "Welcome to the"
+const h2Element = document.createElement('h2');
+h2Element.textContent = 'Welcome to the';
+
+// Skapa ett h1-element för "CirkuS"
+const h1Element = document.createElement('h1');
+h1Element.textContent = 'CirkuS';
+
+// Lägg till h2 och h1 i "enter"-sektionen
+enterSection.appendChild(h2Element);
+enterSection.appendChild(h1Element);
